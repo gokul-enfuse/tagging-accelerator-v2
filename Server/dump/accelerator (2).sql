a21789b4abda796d855713623f22e6ee6fe3cc0f
vikasBoseEnFuse <vikas.bose@enfuse-solutions.com>
2024-04-02 11:30:31 +0530

    adding sql dump file

--- Server/dump/accelerator (2).sql
+++ Server/dump/accelerator (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2024 at 10:51 AM
+-- Generation Time: Apr 02, 2024 at 07:58 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -86,8 +86,12 @@ CREATE TABLE `accelerator_profile` (
 INSERT INTO `accelerator_profile` (`profile_id`, `profile_name`, `profile_email`, `profile_fullname`, `profile_username`, `profile_password`, `profile_confirmpassword`, `profile_role`, `project_id`, `profile_login_session`, `createdDate`, `modifiedDate`) VALUES
 (1, 'null', 'null', 'null', 'admin', 'U2FsdGVkX1/AYMvSSwWS5oZsgz8flaOoBZl8k/YAP40=', 'admin123', 1, '0', 0, '2023-04-16', '0000-00-00'),
 (25, 'Vikas Bose', 'vikas.bose@gmail.com', 'Vikas Bose', 'VikasBose.1562', 'U2FsdGVkX18aJ1kIL5bAodPbooeqzfMbDFGmNGs843Y=', '0r2dunia', 2, '1', 0, '2024-03-01', '2024-03-01'),
-(26, 'saddam', 'saddam@gmail.com', 'saddam', 'saddam.4373', 'U2FsdGVkX18qntNDBv6NJSXQXEMvbPRYtFRNYLNJi8I=', '44pe5alw', 3, ',1', 0, '2024-03-01', '2024-03-01'),
-(28, 'Sunanda', 'sunanda@gmail.com', 'Sunanda', 'Sunanda.7965', 'U2FsdGVkX1+qLg+UvdhntEErdBTRbfAD/QJfwEZI4Yg=', 'ttmv2vfs', 4, '', 0, '2024-03-01', '2024-03-01');
+(26, 'saddam', 'saddam@gmail.com', 'saddam', 'saddam.4373', 'U2FsdGVkX18qntNDBv6NJSXQXEMvbPRYtFRNYLNJi8I=', '44pe5alw', 3, '1,1', 0, '2024-03-01', '2024-03-01'),
+(28, 'Sunanda', 'sunanda@gmail.com', 'Sunanda', 'Sunanda.7965', 'U2FsdGVkX1+qLg+UvdhntEErdBTRbfAD/QJfwEZI4Yg=', 'ttmv2vfs', 4, '', 0, '2024-03-01', '2024-03-01'),
+(29, 'lalitha', 'lalitha@gmail.com', 'lalitha', 'lalitha.5619', 'U2FsdGVkX18DjyHyVhoGhwIgCuMi+3+DIAWzC2s7Vqg=', 'epc5g1rj', 3, '1,1', 0, '2024-03-04', '2024-03-04'),
+(30, 'Venkatesh', 'Venkatesh@gmail.com', 'Venkatesh', 'Venkatesh.3935', 'U2FsdGVkX19VpKrmDBdH+6d8GEbX1zgPsI4Ct9gtkho=', 'aqtrxr72', 4, '', 0, '2024-03-04', '2024-03-04'),
+(31, 'sunil', 'sunil@gmail.com', 'sunil', 'sunil.6771', 'U2FsdGVkX1/nvK3A5CB4v5ADIMrF2UP6jI6q4EsRyPg=', '7x9f2yqd', 2, '2', 0, '2024-03-22', '2024-03-22'),
+(32, 'Neelesh', 'neelesh@gmail.com', 'Neelesh', 'Neelesh.1116', 'U2FsdGVkX1/9tpoYQPVleMKdI31kd+nwVppdpfnmdGA=', 'j7krwtum', 3, ',1,3', 0, '2024-03-30', '2024-03-30');
 
 -- --------------------------------------------------------
 
@@ -144,16 +148,19 @@ INSERT INTO `accelerator_role` (`role_id`, `role_name`) VALUES
 CREATE TABLE `accelerator_tasks` (
   `task_id` int(12) NOT NULL,
   `task_folder_name` varchar(225) NOT NULL,
-  `task_title` varchar(45) NOT NULL,
-  `task_status` varchar(40) NOT NULL,
-  `reviewer_task_status` varchar(8) NOT NULL,
+  `task_title` varchar(225) NOT NULL,
+  `task_status` varchar(40) NOT NULL DEFAULT 'To Do',
+  `reviewer_task_status` varchar(40) NOT NULL,
   `project_id` int(30) NOT NULL COMMENT 'add more project id with common separator',
-  `profile_id` int(5) NOT NULL,
+  `profile_id` int(5) NOT NULL COMMENT 'This id belong to Tagger id',
+  `numOfItemAssignToTagger` int(255) NOT NULL DEFAULT 0,
   `reviewer_profile_id` int(5) NOT NULL,
+  `numOfItemAssignToReviewer` int(225) NOT NULL DEFAULT 0,
   `task_role` int(5) NOT NULL,
   `task_mediatype` varchar(30) NOT NULL,
-  `task_filename` varchar(225) NOT NULL COMMENT 'This field content media type name',
-  `task_filepath` varchar(225) NOT NULL COMMENT 'This field content media type path',
+  `task_zip_folder_name` varchar(225) NOT NULL,
+  `task_freezz` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'This is indication that task is now freezz.',
+  `task_process_type` varchar(50) NOT NULL DEFAULT 'bulk' COMMENT 'This field indicate which kind of process tagger doing it.',
   `createdDate` date NOT NULL DEFAULT current_timestamp(),
   `modifiedDate` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -162,8 +169,11 @@ CREATE TABLE `accelerator_tasks` (
 -- Dumping data for table `accelerator_tasks`
 --
 
-INSERT INTO `accelerator_tasks` (`task_id`, `task_folder_name`, `task_title`, `task_status`, `reviewer_task_status`, `project_id`, `profile_id`, `reviewer_profile_id`, `task_role`, `task_mediatype`, `task_filename`, `task_filepath`, `createdDate`, `modifiedDate`) VALUES
-(1, 'car Bazzar', 'Car_image01', 'Done', '', 1, 26, 0, 3, 'image', '', '', '2024-03-01', '2024-03-02');
+INSERT INTO `accelerator_tasks` (`task_id`, `task_folder_name`, `task_title`, `task_status`, `reviewer_task_status`, `project_id`, `profile_id`, `numOfItemAssignToTagger`, `reviewer_profile_id`, `numOfItemAssignToReviewer`, `task_role`, `task_mediatype`, `task_zip_folder_name`, `task_freezz`, `task_process_type`, `createdDate`, `modifiedDate`) VALUES
+(1, 'folderName_H1C9HGXi3', 'title_H1C9HGXi3', 'Completed', 'Completed', 1, 26, 0, 28, 0, 4, 'image', 'X7jzZWiuP9_extracted', 1, 'bulk', '2024-03-30', '2024-04-01'),
+(2, 'folderName_H1C9HGXi3', 'title_H1C9HGXi3', 'To Do', '', 1, 29, 0, 30, 0, 3, 'image', 'X7jzZWiuP9_extracted', 1, 'bulk', '2024-03-30', '2024-03-30'),
+(3, 'folderName_H1C9HGXi3', 'title_H1C9HGXi3', 'To Do', '', 1, 32, 0, 30, 0, 3, 'image', 'X7jzZWiuP9_extracted', 1, 'bulk', '2024-03-30', '2024-03-30'),
+(4, 'issues', 'Car_image02', 'To Do', '', 3, 32, 0, 0, 0, 3, 'image', 'oiTKl6Ghk_extracted', 0, 'manual', '2024-04-01', '2024-04-01');
 
 -- --------------------------------------------------------
 
@@ -177,8 +187,8 @@ CREATE TABLE `accelerator_task_image` (
   `profile_id` int(10) NOT NULL COMMENT 'assignee profile id ',
   `image_imagename` varchar(50) NOT NULL,
   `image_imagepath` varchar(225) NOT NULL,
-  `createdDate` date NOT NULL,
-  `modifiedDate` date NOT NULL
+  `createdDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modifiedDate` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -186,8 +196,27 @@ CREATE TABLE `accelerator_task_image` (
 --
 
 INSERT INTO `accelerator_task_image` (`image_id`, `task_id`, `profile_id`, `image_imagename`, `image_imagepath`, `createdDate`, `modifiedDate`) VALUES
-(1, 1, 26, 'images-1709313479860.png', 'D:Project - InternalTaggingToolone-dev	aggingTool-V2publicuploadsimagesimages-1709313479860.png', '2024-03-01', '2024-03-01'),
-(2, 1, 26, 'images-1709313479860.png', 'D:Project - InternalTaggingToolone-dev	aggingTool-V2publicuploadsimagesimages-1709313479860.png', '2024-03-01', '2024-03-01');
+(1, 1, 28, '0.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/0.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(2, 1, 28, '120.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/120.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(3, 1, 28, '180.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/180.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(4, 1, 28, '240.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/240.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(5, 1, 28, '300.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/300.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(6, 1, 28, '360.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/360.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(7, 2, 29, '420.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/420.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(8, 2, 29, '480.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/480.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(9, 2, 29, '540.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/540.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(10, 2, 29, '60.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/60.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(11, 2, 29, '600.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/600.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(12, 3, 32, '660.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/660.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(13, 3, 32, '720(1).jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/720(1).jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(14, 3, 32, '720.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/720.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(15, 3, 32, '780.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/780.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(16, 3, 32, '840.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/X7jzZWiuP9_extracted/840.jpg', '2024-03-30 13:26:52', '2024-03-30 13:26:52'),
+(17, 4, 32, 'images-1711965868750.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/manual/images-1711965868750.jpg', '2024-03-31 18:30:00', '2024-04-01 04:35:00'),
+(18, 4, 32, 'images-1711965868762.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/manual/images-1711965868762.jpg', '2024-03-31 18:30:00', '2024-04-01 04:35:00'),
+(19, 4, 32, 'images-1711965868768.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/manual/images-1711965868768.jpg', '2024-03-31 18:30:00', '2024-04-01 04:35:00'),
+(20, 4, 32, 'images-1711965868775.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/manual/images-1711965868775.jpg', '2024-03-31 18:30:00', '2024-04-01 04:35:00'),
+(21, 4, 32, 'images-1711965868782.jpg', 'D:/Project - Internal/TaggingTool/one-dev/taggingTool-V2/public/uploads/images/manual/images-1711965868782.jpg', '2024-03-31 18:30:00', '2024-04-01 04:35:00');
 
 -- --------------------------------------------------------
 
@@ -340,7 +369,7 @@ ALTER TABLE `accelerator_app1url`
 -- AUTO_INCREMENT for table `accelerator_profile`
 --
 ALTER TABLE `accelerator_profile`
-  MODIFY `profile_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `profile_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `accelerator_project`
@@ -358,13 +387,13 @@ ALTER TABLE `accelerator_role`
 -- AUTO_INCREMENT for table `accelerator_tasks`
 --
 ALTER TABLE `accelerator_tasks`
-  MODIFY `task_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `task_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `accelerator_task_image`
 --
 ALTER TABLE `accelerator_task_image`
-  MODIFY `image_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `image_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `rectanglebb`
